bd280af2d68840ceed6dd20f8ca7b6ede533da24
Carmine Zaccagnino <carmine@carminezacc.com>
2020-03-02 19:08:29 +0100

    Initial commit
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+var WebSocket = require('ws');
+var sqlite = require('sqlite3');
+
+var port = process.env.PORT || 3000;
+
+var server = new WebSocket.Server(
+  {
+    port: port,
+  }
+)
+
+let msg = "Server: Welcome!";
+
+server.on('connection', function connection(client) {
+  client.send(msg);
+  client.on('message', function incoming(message) {
+    msg = message;
+    for(var cl of server.clients) {
+      cl.send(message);
+    }
+    console.log("Received the following message:\n" + message);
+  });
+});
